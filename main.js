ca9b84f2c61125f5a048749799248643d6017046
Hiutung <Hiutung85@gmail.com>
2026-05-14 22:26:39 +0200

    修改App图标

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'build/SNGN.ico'),
     webPreferences: {
       nodeIntegration: false, 
       contextIsolation: true, 
